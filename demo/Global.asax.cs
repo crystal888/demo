f0ad779b2970aba9f8a6351dcb33088ad7af00ac
crystal888 <zz342833790@163.com>
2014-11-09 13:37:22 +0800

    99

--- demo/Global.asax.cs
+++ demo/Global.asax.cs
@@ -9,7 +9,7 @@ using System.Web.Routing;
 
 namespace demo
 {
-    // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，111
+    // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，222
     // 请访问 http://go.microsoft.com/?LinkId=9394801
 
     public class MvcApplication : System.Web.HttpApplication
